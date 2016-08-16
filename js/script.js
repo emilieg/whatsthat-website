b7effb435071bc4932c95c6d180adea318551d51
keidra <keidrabaldwin@gmail.com>
2016-08-16 08:59:06 -0500

    removing files, starting static page

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-$(document).ready(function(){
-  $("#demosMenu").change(function(){
-    window.location.href = $(this).find("option:selected").attr("id") + '.html';
-  });
+// $(document).ready(function(){
+//   $("#demosMenu").change(function(){
+//     window.location.href = $(this).find("option:selected").attr("id") + '.html';
+//   });
 
-var words = ['Monosodium glutamate', 'Carmine', 'Maltodextrin', 'Casein', 'Maltose'],
-currentWord = 0;
-
-
-function loop(){
-  $('h2').delay(4000).fadeTo(400,0,function(){
-     $(this).text( words[++currentWord%words.length] + '?').fadeTo(400,1,loop);
-  });
-}
-
-loop();
+// var words = ['Monosodium glutamate', 'Carmine', 'Maltodextrin', 'Casein', 'Maltose'],
+// currentWord = 0;
 
 
-});
+// function loop(){
+//   $('h2').delay(4000).fadeTo(400,0,function(){
+//      $(this).text( words[++currentWord%words.length] + '?').fadeTo(400,1,loop);
+//   });
+// }
+
+// loop();
+
+
+// });
 
 
 
